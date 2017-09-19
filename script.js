6c983414059b4ba76a2aeba8a51a04567595b88b
Marcin Grzeniewski <MarcinGrzeniewski@MacBook-Air-Marcin.local>
2017-09-19 18:09:10 +0200

    Trainglearea

---
+++ script.js
@@ -0,0 +1,13 @@
+var a = 5, h = 6, triangleArea;
+
+triangleArea = a*h/2;
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+lub
+
+var a = prompt"podaj a", h = prompt"podaj h", triangleArea;
+
+triangleArea = a*h/2;
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
